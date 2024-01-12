044896ed9ebe9738c5ef4872fbaa64b4619a02af
Anderson Viana <andersonp.viana.contato@gmail.com>
2024-01-12 20:17:09 -0300

    Feat - calling the email service to send the email

--- ms-email/src/main/java/com/anderson/msemail/consumer/EmailConsumer.java
+++ ms-email/src/main/java/com/anderson/msemail/consumer/EmailConsumer.java
@@ -1,6 +1,8 @@
 package com.anderson.msemail.consumer;
 
 import com.anderson.msemail.dtos.EmailDTO;
+import com.anderson.msemail.entities.Email;
+import com.anderson.msemail.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -8,8 +10,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailConsumer {
 
+    private final EmailService emailService;
+
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailDTO data) {
-
+        var email = new Email(data);
+        this.emailService.sendEmail(email);
     }
 }
