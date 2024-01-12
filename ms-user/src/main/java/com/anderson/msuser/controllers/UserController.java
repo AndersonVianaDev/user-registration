fdbbb74f584999c60bb63f01459ea732c74bec08
Anderson Viana <andersonp.viana.contato@gmail.com>
2024-01-12 11:40:48 -0300

    Feat - "Feat - Login created"

--- ms-user/src/main/java/com/anderson/msuser/controllers/UserController.java
+++ ms-user/src/main/java/com/anderson/msuser/controllers/UserController.java
@@ -1,10 +1,16 @@
 package com.anderson.msuser.controllers;
 
+import com.anderson.msuser.dtos.LoginDTO;
+import com.anderson.msuser.dtos.LoginResponseDTO;
 import com.anderson.msuser.dtos.RegisterDTO;
+import com.anderson.msuser.entities.User;
+import com.anderson.msuser.infra.security.TokenService;
 import com.anderson.msuser.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +22,29 @@ public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService) {
+    private final AuthenticationManager authenticationManager;
+
+    private final TokenService tokenService;
+
+    public UserController(UserService userService, AuthenticationManager authenticationManager, TokenService tokenService) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
     }
 
-    @PostMapping
+    @PostMapping("/register")
     public ResponseEntity register(@RequestBody @Valid RegisterDTO data) {
         this.userService.createUser(data);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid LoginDTO data) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+        var loginResponseDTO = new LoginResponseDTO(token);
+
+        return ResponseEntity.status(HttpStatus.OK).body(loginResponseDTO);
     }
 }
